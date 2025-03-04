337582b693a697d0de9e1e2bec1ab4a1a9fc0f8c
shinchan2007 <kishorsuresh210@gmail.com>
2025-03-04 20:45:56 +0530

    new files

--- src/TravelDetails/Traveldetails.jsx
+++ src/TravelDetails/Traveldetails.jsx
@@ -8,16 +8,16 @@ export default function Traveldetails() {
     const Date = UseZustand((state)=>state.Date)
     const SetDate = UseZustand((state)=>state.SetDate);
     const DarkMode = UseZustand((state)=>state.Mode);
-
+    const pattern = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+    const [Iscorrect,Setiscorrect] = useState(true)
   return (
     <View style={[styles.traveldetailsbody,{backgroundColor:DarkMode?"rgb(59, 59, 59)":"rgb(255, 255, 255)"}]}>
         <Text style={[styles.destitle,{color:DarkMode?"rgb(255, 255, 255)":"rgb(0, 0, 0)"}]}>Travel Details</Text>
 
         {/* input-1 */}
         <KeyboardAvoidingView behavior='padding'>
-
         <View style={[styles.inputholder,{backgroundColor:DarkMode?"rgb(93, 93, 93)":"rgb(255, 255, 255)"}]}>
-        <Text style={styles.label}>Destination</Text>
+        <Text style={styles.label}>{"Destination"}</Text>
         <TextInput 
          style={[styles.input1,{color:DarkMode?"rgb(255, 255, 255)":"rgb(0, 0, 0)"}]}
          placeholder='Where do you want to go?'
@@ -28,13 +28,17 @@ export default function Traveldetails() {
         </View>
 
         {/* input-2 */}
-        <View style={[styles.inputholder,{backgroundColor:DarkMode?"rgb(93, 93, 93)":"rgb(255, 255, 255)"}]}>
+        <View style={[styles.inputholder,{backgroundColor:DarkMode?"rgb(93, 93, 93)":"rgb(255, 255, 255)",
+          borderColor:Date.length>0 && !Iscorrect?"red":"rgb(206, 206, 206)"}]}>
         <Text  style={styles.label}>Travel Date</Text>
-        <TextInput style={[styles.input1,{color:DarkMode?"rgb(255, 255, 255)":"rgb(0, 0, 0)"}]} 
+        <TextInput style={[styles.input1,{color:DarkMode?"rgb(255, 255, 255)":"rgb(0, 0, 0)"
+        }]} 
         placeholder='MM/DD/YYYY'
         placeholderTextColor={'rgb(160, 159, 159)'}
         value={Date}
-        onChangeText={(text)=>{SetDate(text)}}
+        onChangeText={(text)=>{SetDate(text);
+          const isValid = text.length > 0 ? pattern.test(text) : true;
+          Setiscorrect(isValid)}}
         ></TextInput>
         </View>
         </KeyboardAvoidingView>
